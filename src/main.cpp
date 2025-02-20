6fc0871ad32aeea0c9bd63493fdfcf32e456b075
bebraaaa1337 <artur.filatov3010200@gmail.com>
2025-02-20 15:42:10 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 
 extern int add(int a, int b);
 extern int subtract(int a, int b);
-
+extern int multiply(int a, int b)
 int main() {
     int a = 5, b = 4;
     std::cout << "5 + 4 = " << add(a, b) << std::endl;
     std::cout << "5 - 4 = " << subtract(a, b) << std::endl;
+    std::cout << "5 * 3 = " << multiply(5, 3) << std::endl;
     return 0;
 }
