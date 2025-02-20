57d99f96873b72d054fe60d55c9e464af4874f36
bebraaaa1337 <artur.filatov3010200@gmail.com>
2025-02-20 15:10:38 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+extern int add(int a, int b);
+extern int subtract(int a, int b);
+
+int main() {
+    int a = 5, b = 4;
+    std::cout << "5 + 4 = " << add(a, b) << std::endl;
+    std::cout << "5 - 4 = " << subtract(a, b) << std::endl;
+    return 0;
+}
